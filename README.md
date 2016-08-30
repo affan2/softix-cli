b8da6616e28eee17d83e91790adc651d2d50a6e4
Matt Chung <matt@itsmemattchung.com>
2016-08-29 21:04:44 -0700

    Updated readme and output response to JSON

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## Creating new customer
 
-```python
-softix customer add -d '{"firstname": "joe", "lastname": "bob"}'
-softix customer add --data '{"firstname": "joe", "lastname": "bob"}'
+```bash
+
+$ softix create-customer --data '{"internationalcode": "971", "firstname": "ajilan", "areacode": "unknown", "lastname": "MA", "phonenumber": "507156120", "salutation": "-", "nationality": "IN", "city": "dubai", "countrycode": "IN", "dateofbirth": "4-23-2015", "state": "dubai", "addressline2": "-", "addressline3": "-", "addressline1": "-", "email": "unknown@unknown.com"}'
+{"id": 1301777}
+
 ```
--- softixcli/cli.py
+++ softixcli/cli.py
@@ -4,9 +4,9 @@ import softix
 
 
 @click.group()
-@click.option('--client-id', envvar='SOFTTIX_CLIENT_ID')
-@click.option('--secret', envvar='SOFTTIX_SECRET')
-@click.option('--seller-code', envvar='SOFTTIX_SELLER_CODE')
+@click.option('--client-id', help='Client ID', envvar='SOFTIX_CLIENT_ID')
+@click.option('--secret', help='Secret', envvar='SOFTIX_SECRET')
+@click.option('--seller-code', help='Seller code', envvar='SOFTIX_SELLER_CODE')
 @click.pass_context
 def cli(context, client_id, secret, seller_code):
     context.obj = {}
@@ -19,13 +19,22 @@ def cli(context, client_id, secret, seller_code):
 @click.option("--data", "-d", help="Customer data as json", required=True)
 @click.pass_context
 def create_customer(context, data):
-    """Create a new customer."""
+    """
+    Create a new customer.
+    """
     softix_client = softix.SoftixCore()
     softix_client.authenticate(context.obj['client_id'], context.obj['secret'])
+    seller_code = context.obj['seller_code']
     try:
-        json.loads(data)
+        customer_data = json.loads(data)
+        customer_id = softix_client.create_customer(seller_code, **customer_data)
+        output = json.dumps({'id': customer_id})
+        click.echo(output)
     except ValueError:
         click.echo("Unable to parse customer data as JSON")
+        context.exit(1)
+    except softix.exceptions.SoftixError as error:
+        click.echo(error.message)
         context.exit(1)
 
 
